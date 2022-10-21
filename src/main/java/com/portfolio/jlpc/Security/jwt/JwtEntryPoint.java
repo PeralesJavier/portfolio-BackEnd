cfea0c5f014ed206330093f66a3708d0a3f7d20f
Javier <javier.perales175962@alumnos.info.unlp.edu.ar>
2022-10-21 18:22:41 -0300

    Creando he implementando los metodos dentro del package jwt

--- src/main/java/com/portfolio/jlpc/Security/jwt/JwtEntryPoint.java
+++ src/main/java/com/portfolio/jlpc/Security/jwt/JwtEntryPoint.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.portfolio.jlpc.Security.jwt;
 
-/**
- *
- * @author Javier
- */
-public class JwtEntryPoint {
+//Va a comprobar si hay un token valido; sino devuelve una respuesta 401 no autorizado
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class JwtEntryPoint implements AuthenticationEntryPoint{
+
+    //layer utilizado en desarrollo para ver el metodo que este dando error
+    //en caso que la app no funcione
+    private final static Logger logger = LoggerFactory.getLogger(JwtEntryPoint.class);
+    
+    @Override
+    public void commence(HttpServletRequest req, HttpServletResponse res, AuthenticationException e) throws IOException, ServletException {
+        //aca lanzamos el error
+        //Esto solo se utiliza en el desarrollo cuando la app esta lista no hay que tenerlo
+        logger.error("Fail en el metodo commence");
+        // el res envia un error; rechaza todas las peticiones que no esten autenticadas
+        //rechaza todas las peticiones que no esten autenticadas, manda SC_UNAUTHORIZED
+        res.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
     
 }
--- src/main/java/com/portfolio/jlpc/Security/jwt/JwtTokenFilter.java
+++ src/main/java/com/portfolio/jlpc/Security/jwt/JwtTokenFilter.java
@@ -1,13 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.portfolio.jlpc.Security.jwt;
 
-/**
- *
- * @author Javier
- */
-public class JwtTokenFilter {
+//Se va a ejecutar por cada peticion; va a comprobar que sea valido el token utilizacon el JwtProvider
+
+import com.portfolio.jlpc.Security.service.UserDetailsServiceImpl;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+//En caso que sea valido el token va a permitir el acceso al recurso
+//en caso contrario lanzara la excepcion
+
+public class JwtTokenFilter extends OncePerRequestFilter{
+    
+    private final static Logger logger = LoggerFactory.getLogger(JwtTokenFilter.class);
+    @Autowired
+    JwtProvider JwtProvider;
+    
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+            
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
+        //se ejecuta una vez por cada peticion; comprueba que el token es valido
+        //utilizando al provider
+        try {
+            String token = getToken(req);
+            if(token != null && JwtProvider.validateToken(token)){
+                String nombreUsuario = JwtProvider.getNombreUsuarioFromToken(token);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(nombreUsuario);
+                UsernamePasswordAuthenticationToken auth =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, 
+                        userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        } catch(Exception e){
+            logger.error("fail en el metodo doFilter");
+        }
+        filterChain.doFilter(req, res);
+    }
+    
+    private String getToken(HttpServletRequest request){
+        String header = request.getHeader(ALREADY_FILTERED_SUFFIX);
+        if(header != null && header.startsWith("Bearer"))
+            return header.replace("Bearer", "");
+        return null;
+    }
     
 }
