dca32b4f1ba486f68e1eab3112cc19a1eb992c4f
Javier <javier.perales175962@alumnos.info.unlp.edu.ar>
2022-10-31 01:02:10 -0300

    corrigiendo errores cors

--- src/main/java/com/portfolio/jlpc/controller/CExperiencia.java
+++ src/main/java/com/portfolio/jlpc/controller/CExperiencia.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendargportfolio.web.app/")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
